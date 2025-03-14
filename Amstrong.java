0ba48e02d7a5f382643ec69e246d7f12d3210d40
MatthO12 <23124029@srcas.ac.in>
2025-03-14 10:41:55 +0530

    Amstrong.java

---
+++ Amstrong.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SimpleArmstrong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        
+        int originalNumber = number;
+        int result = 0;
+        
+        while (originalNumber != 0) {
+            int remainder = originalNumber % 10;
+            result += remainder * remainder * remainder;
+            originalNumber /= 10;
+        }
+        
+        if (result == number) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+}
